477ad8946f3d435b63456ae78a6147351ec1bd77
liu-ziting <liuziting2014@163.com>
2020-01-09 16:00:13 +0800

    完善全部页面

--- js/main.js
+++ js/main.js
@@ -217,3 +217,14 @@ document.onreadystatechange = function(){
 
    　 }
 }
+
+//禁止F12,//禁止右键
+$(document).bind("contextmenu", function () { return false; });
+	document.oncontextmenu = function () { return false; };
+	document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = false;
+        return false;
+    }
+};
